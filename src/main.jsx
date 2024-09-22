2dbf6faf1c9db296eaa190384bd38f8e795ad67d
Decsis <a@MacBook-Pro-A.local>
2024-09-22 21:52:39 +0300

    final update

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename={`/${process.env.PUBLIC_URL}`}>
+    <BrowserRouter basename="/QR-code-App/">
       <App />
     </BrowserRouter>
   </StrictMode>,
--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: "https://decsiss.github.io/QR-code-App/",
+  base: "/QR-code-App/",
 })
